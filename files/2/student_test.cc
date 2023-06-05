2d56bc9080c285b2d3cfb1327ff1ace760d94da0
Tejas <tejas8acharya@gmail.com>
2023-06-05 20:40:10 +0530

    Add DFS tests

--- files/2/student_test.cc
+++ files/2/student_test.cc
@@ -8,3 +8,44 @@
 // Write some test cases for each function.
 //-----------------------------------------------------------------------------
 
+TEST(DFS, EmptyGraph) {
+    std::map<int, std::set<int>> adjacency_list;
+    Graph g(adjacency_list);
+    std::vector<int> expected;
+    EXPECT_EQ(g.DFS(0), expected);
+}
+
+TEST(DFS, SingleNodeNoEdge) {
+    std::map<int, std::set<int>> adjacency_list = {{0, {}}};
+    Graph g(adjacency_list);
+    std::vector<int> expected = {0};
+    EXPECT_EQ(g.DFS(0), expected);
+}
+
+TEST(DFS, SingleNodeSingleEdge) {
+    std::map<int, std::set<int>> adjacency_list = {{0, {0}}};
+    Graph g(adjacency_list);
+    std::vector<int> expected = {0};
+    EXPECT_EQ(g.DFS(0), expected);
+}
+
+TEST(DFS, TwoNodeUnconnected) {
+    std::map<int, std::set<int>> adjacency_list = {{0, {}}, {1, {}}};
+    Graph g(adjacency_list);
+    std::vector<int> expected = {0};
+    EXPECT_EQ(g.DFS(0), expected);
+}
+
+TEST(DFS, TwoNodeConnected) {
+    std::map<int, std::set<int>> adjacency_list = {{0, {1}}, {1, {0}}};
+    Graph g(adjacency_list);
+    std::vector<int> expected = {0, 1};
+    EXPECT_EQ(g.DFS(0), expected);
+}
+
+TEST(DFS, MultipleNodeConnected) {
+    std::map<int, std::set<int>> adjacency_list = {{0, {1, 2, 3}}, {1, {0}}, {2, {0, 3}}, {3, {0, 2}}};
+    Graph g(adjacency_list);
+    std::vector<int> expected = {0, 1, 2, 3};
+    EXPECT_EQ(g.DFS(0), expected);
+}
