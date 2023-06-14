eff87fd17ace3baf4267761982d6faa1cffcfb54
Tejas <tejas8acharya@gmail.com>
2023-06-14 08:43:12 +0530

    Add SingleNodeSingleEdgePath test for IsPathBetweenNodes

--- files/2/student_test.cc
+++ files/2/student_test.cc
@@ -120,3 +120,9 @@ TEST(DFSAll, SingleNodeSingleEdgeNoPath) {
     Graph g(adjacency_list);
     EXPECT_FALSE(g.IsPathBetweenNodes(0, 1));
 }
+
+TEST(DFSAll, SingleNodeSingleEdgePath) {
+    std::map<int, std::set<int>> adjacency_list = {{0, {0}}};
+    Graph g(adjacency_list);
+    EXPECT_TRUE(g.IsPathBetweenNodes(0, 0));
+}
