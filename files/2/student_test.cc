0f973f78bc52a7c4ca21493ab87daeb835a2ade5
Tejas <tejas8acharya@gmail.com>
2023-06-14 08:46:59 +0530

    Add MultipleConnectedNodeNoPath test for IsPathBetweenNodes

--- files/2/student_test.cc
+++ files/2/student_test.cc
@@ -138,3 +138,9 @@ TEST(IsPathBetweenNodes, TwoNodeConnected) {
     Graph g(adjacency_list);
     EXPECT_TRUE(g.IsPathBetweenNodes(0, 1));
 }
+
+TEST(IsPathBetweenNodes, MultipleNodeConnectedNoPath) {
+    std::map<int, std::set<int>> adjacency_list = {{0, {1, 2, 3}}, {1, {0}}, {2, {0, 3}}, {3, {0, 2}}};
+    Graph g(adjacency_list);
+    EXPECT_TRUE(g.IsPathBetweenNodes(3, 1));
+}
